35c80162949de846d076c6c1cea72e28f718f32c
vijay vekariya <vijayvekariya007@gmail.com>
2024-01-31 16:35:49 +0530

    31-1-24

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Nidham Kacha'),
     );
   }
 }
@@ -36,7 +36,7 @@ class _MyHomePageState extends State<MyHomePage> {
     setState(() {
       _counter++;
     });
-  }
+  } 
 
   @override
   Widget build(BuildContext context) {
