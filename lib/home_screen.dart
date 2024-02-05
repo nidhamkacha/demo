b23b3b18edef95f137004a92f31e91a46e0a6b2f
vijay vekariya <vijayvekariya007@gmail.com>
2024-02-05 15:11:30 +0530

    5-2-24

---
+++ lib/home_screen.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: Icon(Icons.account_circle_outlined),
+        // leadingWidth: 100,
+        centerTitle: true,
+        title: Text("Flutter Application"),
+        backgroundColor: Colors.red,
+        shadowColor: Colors.black,
+        actions: [
+          IconButton(onPressed: () {}, icon: Icon(Icons.search)),
+          IconButton(onPressed: () {}, icon: Icon(Icons.more_vert)),
+        ],
+        titleTextStyle: TextStyle(fontSize: 22, color: Colors.black),
+        bottom: TabBar(tabs: 
+        [
+          Tab(icon: Icons.home)
+        ]
+        ),
+      ),
+      body: Text("Nidham"),
+    );
+  }
+}
